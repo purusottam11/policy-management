dcb398e81ea67fadd962d9de6af186acfee55225
Purusottam Mohapatra <purusottammohapatra@Purusottams-MacBook-Pro.local>
2022-07-21 23:42:23 +0530

    code clean up

---
+++ src/main/java/com/embea/policymanagement/controller/PolicyController.java
@@ -0,0 +1,34 @@
+package com.embea.policymanagement.controller;
+
+import com.embea.policymanagement.dto.*;
+import com.embea.policymanagement.service.PolicyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+@RequestMapping("/policy")
+@RestController
+public class PolicyController {
+
+    private PolicyService policyService;
+
+    @PostMapping("/create")
+    public ResponseEntity<PolicyCreationResponseDto> createPolicy(@RequestBody PolicyCreationRequestDto policyCreationRequestDto) {
+        return new ResponseEntity<>(policyService.createPolicy(policyCreationRequestDto), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/modify")
+    public ResponseEntity<PolicyModificationResponseDto> modifyPolicy(@RequestBody PolicyModificationRequestDto policyModificationRequestDto) {
+        return new ResponseEntity<>(policyService.modifyPolicy(policyModificationRequestDto), HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{policyId}/{requestDate}")
+    public ResponseEntity<PolicyInformationResponseDto> getPolicy(@PathVariable(name = "policyId") String policyId, @PathVariable(name = "requestDate") Date requestDate) {
+        return new ResponseEntity<>(policyService.getPolicy(policyId, requestDate), HttpStatus.OK);
+    }
+
+
+}
